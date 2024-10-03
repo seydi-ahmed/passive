676b95432ee2a6486e9df5609fcba517a33ece0e
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-03 22:38:28 +0000

    new folder

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "explication dans les détails"
+git commit -m "new folder"
 git push
