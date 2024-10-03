1d712509db3e52d5ca76d67c082e7d73436e216c
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-03 22:44:17 +0000

    explication.txt updated

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "new folder"
+git commit -m "explication.txt updated"
 git push
