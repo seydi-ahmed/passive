9e5459d33e70e92d14345ccdab620607d7fa5b51
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-04 07:37:29 +0000

    resultat.txt and resultat2.txt deleted

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "resultat.txt and resultat2.txt erased"
+git commit -m "resultat.txt and resultat2.txt deleted"
 git push
