94968e5455bfe09dc702bd9f6ee11e89544fa1f0
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-07 07:21:42 +0000

    result of the ip adress

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "explication updated"
+git commit -m "result of the ip adress"
 git push
