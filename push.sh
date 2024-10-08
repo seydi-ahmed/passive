fadf7d297ee0f4aea3537f13b6c5b5f9d2e43c86
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-08 11:27:53 +0000

    json

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "help message"
+git commit -m "json"
 git push
