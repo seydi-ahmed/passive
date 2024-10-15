fb707f0ee5cf0d53af0b36f56fffe293b1a52ff4
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-15 21:11:41 +0000

    result

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "article"
+git commit -m "result"
 git push
