9cc9994a555a748832e940ca5f485f50fcfcc018
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-15 21:15:40 +0000

    third result

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "second result"
+git commit -m "third result"
 git push
